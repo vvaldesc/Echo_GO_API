2e891531d5bb5f4f96d8a73d108baa85802e9a69
vvaldesc <victor.valdescobos@riberadeltajo.es>
2024-06-27 13:38:21 +0200

    Static html rendering

--- routes/routes.go
+++ routes/routes.go
@@ -1,15 +1,43 @@
 package routes
 
 import (
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
+type TemplateRender struct {
+	templates *template.Template
+}
+
+func (t *TemplateRender) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func Routes(e *echo.Echo) {
+
+	renderer := &TemplateRender{
+		templates: template.Must(template.ParseGlob("static/templates/*.html")),
+	}
+
+	e.Renderer = renderer
+
 	e.GET("/", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, map[string]string{
-			"message": "Hello, World!",
+		return c.Render(http.StatusOK, "index.html", map[string]string{
+			"Title":   "My Go Web App",
+			"Heading": "Hello, World!",
+			"Message": "This is a simple Go web app.",
 		})
 	})
+
+	e.GET("/ping", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{
+			"message": "pong",
+		})
+	})
+
+	e.Static("/static", "static")
+
 }
